488abd10847fdb9bcdfe7abcb108641be1768b24
Kyungseo-Park <troublesome.dev@gmail.com>
2023-05-03 22:09:44 +0900

    Fix res 라는 파라메터를 넘기지 않아서, res.cookies 가 undefind 가 나옴

--- src/controller/logout-control.js
+++ src/controller/logout-control.js
@@ -2,7 +2,7 @@ import logoutService from "../services/logout-service.js";
 
 const logoutControl = async (req, res, next) => {
   try {
-    const processResult = await logoutService();
+    const processResult = await logoutService(req, res);
     return res.status(processResult.status).send(processResult.message);
   } catch (err) {
     next(err);
