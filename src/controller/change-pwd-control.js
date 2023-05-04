ecbe2af02d513bc8fb74a81e0813f36735c1766d
lee <dlckdgus200011@gmail.com>
2023-05-04 19:43:32 +0900

    Fix: token fix

--- src/controller/change-pwd-control.js
+++ src/controller/change-pwd-control.js
@@ -2,7 +2,7 @@ import changePwdService from "../services/change-pwd-service.js";
 
 const changePwdCtrl = async (req, res, next) => {
   try {
-    const { token } = req.cookies.token;
+    const token = req.cookies.token;
     const { currentPwd, newPwd } = req.body;
     const processResult = await changePwdService(token, currentPwd, newPwd);
     return res.status(processResult.statusCode).send(processResult.message);
--- src/controller/user-delete-control.js
+++ src/controller/user-delete-control.js
@@ -2,7 +2,7 @@ import userDeleteService from "../services/user-delete-service.js";
 
 const userDeleteCtrl = async (req, res, next) => {
   try {
-    const { token } = req.cookies.token;
+    const token = req.cookies.token;
     const { password } = req.body;
     const processResult = await userDeleteService(token, password);
     return res.status(processResult.statusCode).send(processResult.message);
