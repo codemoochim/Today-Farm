a51e55ae6f11f5358b2ee5a26a751c057f53d572
sando <codemoochim@gmail.com>
2023-05-10 18:26:37 +0900

    Fix: 유저 정보 객체형태로 응답

--- src/controller/userController/user-info-control.js
+++ src/controller/userController/user-info-control.js
@@ -3,7 +3,7 @@ import { userInfoService } from "../../services/userService/index.js";
 const userInfoControl = async (req, res) => {
   const email = req.user;
   const processResult = await userInfoService(email);
-  return res.status(processResult.statusCode).send(processResult.message);
+  return res.status(processResult.statusCode).send(processResult.message[0]);
 };
 
 export default userInfoControl;
